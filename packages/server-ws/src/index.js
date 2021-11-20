501eda5b2d66302f19897ccab0b12337838f63c0
vlshell <vince.lynch@shell.com>
2021-11-20 16:45:13 +0000

    style: [#1] add monorepo, prettier and eslint

--- packages/server-ws/src/index.js
+++ packages/server-ws/src/index.js
@@ -1,55 +1,49 @@
 /** @format */
-const express = require("express");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-const { doSearch } = require('./swapi-api')
-
+const express = require('express')
+const { createServer } = require('http')
+const { Server } = require('socket.io')
+const { doSearch } = require('./swapi-api.js')
 
 /**
  * Topic for incoming search queries
  */
-const TOPIC_SEARCH = "search";
-
+const TOPIC_SEARCH = 'search'
 
 // Constants
-const PORT = 3000;
-const HOST = "0.0.0.0";
-const app = express();
-const httpServer = createServer(app);
-const server = new Server(httpServer);
+const PORT = 3000
+const HOST = '0.0.0.0'
+const app = express()
+const httpServer = createServer(app)
+const server = new Server(httpServer)
 
-server.on("connection", (socket) => {
-  console.log("got connection");
+server.on('connection', (socket) => {
+  console.log('got connection')
 
   socket.on(TOPIC_SEARCH, async (message) => {
-    const matches = await doSearch(message.query);
-    const timer = ms => new Promise(res => setTimeout(res, ms))
-    // console.log(JSON.stringify(matches, null, 2));
-    for(let i = 0; i<matches.length; i++){
-      if(matches[i].delay){
-        // console.log(JSON.stringify(matches[i], null, 2));
-        const {delay, ...resultWithoutDelay} = {...matches[i]};
-        socket.emit(TOPIC_SEARCH, resultWithoutDelay);
-        // await timer(matches[i].delay || 0);
-        await timer(delay || 0);
+    const matches = await doSearch(message.query)
+    const timer = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+    // Console.log(JSON.stringify(matches, null, 2));
+    matches.forEach(async (match) => {
+      if (match.delay) {
+        // Console.log(JSON.stringify(match, null, 2));
+        const { delay, ...resultWithoutDelay } = { ...match }
+        socket.emit(TOPIC_SEARCH, resultWithoutDelay)
+        // Await timer(match.delay || 0);
+        await timer(delay || 0)
+      } else {
+        socket.emit(TOPIC_SEARCH, match)
       }
-      else{
-        socket.emit(TOPIC_SEARCH, matches[i]);
-      }
-    }
-  });
+    })
+  })
 
+  socket.on('disconnect', () => {
+    console.log(`lost connection with ${socket.id}`)
 
+    // Console.log(server.sockets.sockets.values());
+  })
 
-  socket.on("disconnect", () => {
-    console.log(`lost connection with ${socket.id}`);
+  console.log(`connection established with ${socket.id}`)
+})
 
-    // console.log(server.sockets.sockets.values());
-  });
-
-  console.log(`connection established with ${socket.id}`);
-});
-
-
-console.log(`Running on http://${HOST}:${PORT} (env: ${process.env.NODE_ENV})`);
-httpServer.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT} (env: ${process.env.NODE_ENV})`)
+httpServer.listen(PORT, HOST)
