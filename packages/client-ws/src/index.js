353a2aaeda8a9ae2655b152f75d66be08ed65993
vince-lynch <autovincelynch@gmail.com>
2021-11-20 17:53:39 +0000

    feat(client-ws): [#6] make example query from client-ws

--- packages/client-ws/src/index.js
+++ packages/client-ws/src/index.js
@@ -2,6 +2,16 @@ const { io } = require('socket.io-client')
 
 const socket = io('http://localhost:3000')
 
+const message = {
+  query: 'Luke'
+}
+
 socket.on('connect', () => {
   console.log(socket.id)
+
+  socket.emit('search', message)
+
+  socket.on('search', (data) => {
+    console.log('data', data)
+  })
 })
